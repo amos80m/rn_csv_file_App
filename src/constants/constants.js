0b6b013480975ca64e6c263035aebf6ea17bc6b4
Amos Megidish <amosos190@gmail.com>
2018-08-02 01:09:52 +0300

    clone & yarn

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,2 @@
-export const URL_PREFIX = 'http://localhost:3000'
-// export const URL_PREFIX = 'https://excele.herokuapp.com'
+// export const URL_PREFIX = 'http://localhost:3000'
+export const URL_PREFIX = 'https://excele.herokuapp.com'
